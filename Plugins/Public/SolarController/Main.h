e4e97c967f74dfa3f42ad324299d6b930f1c2f3e
Conrad Weiser <bluecrecent0@gmail.com>
2018-03-23 21:07:37 -0400

    Stuff I guess

--- Plugins/Public/SolarController/Main.h
+++ Plugins/Public/SolarController/Main.h
@@ -12,9 +12,42 @@
 #include <FLHook.h>
 #include <plugin.h>
 #include <PluginUtilities.h>
+#include "SpaceObject.h"
 
 using namespace std;
 
+////////////////////////////////////////////
+//A list of each existing type of object
+////////////////////////////////////////////
+map<uint, SpaceObject*> spaceObjects;
 
+////////////////////////////////////////////
+//BROAD NAMESPACE DECLARATIONS FOR OBJECTS
+////////////////////////////////////////////
+namespace ExportData
+{
+	void ToHTML();
+	void toJSON();
+}
+
+namespace Log
+{
+	void LogBaseAction(string basename, const char *message);
+	void LogGenericAction(string message);
+}
+
+namespace SolarPlayerCmd
+{
+	void SolarHelp(uint client, const wstring &args);
+	void SolarDeploy(uint client, const wstring &args);
+}
+
+namespace AdminCommands
+{
+	void AdminHelp(uint client, const wstring &args);
+}
+
+// Are we in debugging mode for the plugin?
+extern bool debuggingMode;
 
 #endif
